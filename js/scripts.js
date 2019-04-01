45d1da14b78644ba36a259a847b0f2bdad738bef
megan schulte and Nathan Aden <nathan@>
2019-04-01 11:46:45 -0700

    add looped array to populate ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+var flavors = ["chocolate", "vanilla", "chunky monkey", "rum raisin", "americone dream"];
+
 $(document).ready(function () {
 
+  flavors.forEach(function(flavor) {
+    $("#listOf_iceCreamFlavors").append("<li>" + flavor + "</li>");
+
+  
+  });
 });
