5273ff411bc0b1ca8f7c502cb156ee257b85923a
Heather Zurek and megan schulte <heather@>
2019-04-02 10:21:04 -0700

    change forEach loop to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 var flavors = ["chocolate", "vanilla", "chunky monkey", "rum raisin", "americone dream"];
 
-$(document).ready(function () {
+$(document).ready(function() {
+console.log("working");
+  for (var index = 0; index < flavors.length; index ++) {
 
-  flavors.forEach(function(flavor) {
-    $("#listOf_iceCreamFlavors").append("<li>" + flavor + "</li>");
-
-  
-  });
+  // flavors.forEach(function(flavor) {
+    $("#listOf_iceCreamFlavors").append("<li>" + flavors[index] + "</li>"); console.log(flavors[index]);
+  };
+  $("#listOf_iceCreamFlavors").show();
 });
